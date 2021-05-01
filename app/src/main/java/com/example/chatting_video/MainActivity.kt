37a5ac37d099e3c166e18877bf11c4c295ce6e01
EomJunyoung <theoneday737@naver.com>
2021-05-01 17:48:41 +0900

    --------------------------------------------------

--- app/src/main/java/com/example/chatting_video/MainActivity.kt
+++ app/src/main/java/com/example/chatting_video/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //test
+        //test test
         auth = FirebaseAuth.getInstance()
 
         binding.loginButtonMain.setOnClickListener{
