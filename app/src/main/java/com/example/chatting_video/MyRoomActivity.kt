0509dd2fb1939fb213b93aa972298a2324654930
EomJunyoung <theoneday737@naver.com>
2021-05-05 18:41:11 +0900

    --------------------------------------------------

--- app/src/main/java/com/example/chatting_video/MyRoomActivity.kt
+++ app/src/main/java/com/example/chatting_video/MyRoomActivity.kt
@@ -3,7 +3,7 @@ package com.example.chatting_video
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//내 대화리스트 확인할 수 있는 곳
+//내 대화리스트 확인할 수 있는 곳--
 class MyRoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
